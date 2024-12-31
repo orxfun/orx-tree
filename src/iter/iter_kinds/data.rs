21bd179438cdf64de1d2ac01a97af2b5b25de217
orxfun <orx.ugur.arikan@gmail.com>
2024-12-31 09:16:28 +0100

    iter kind documentation tests added

--- src/iter/iter_kinds/data.rs
+++ src/iter/iter_kinds/data.rs
@@ -9,6 +9,7 @@ use orx_selfref_col::{MemoryPolicy, NodePtr, SelfRefCol};
 
 // core
 
+/// Iterator over values obtained from tree nodes.
 pub struct NodeVal<D>(PhantomData<D>);
 
 impl<'a, V, M, P, D> IterKindCore<'a, V, M, P> for NodeVal<D>
@@ -40,6 +41,74 @@ where
 
 // over
 
+/// Iterator over data or values of the nodes.
+///
+/// # Examples
+///
+/// ```
+/// use orx_tree::*;
+/// use orx_tree::iter::*;
+///
+/// //      1
+/// //     ╱ ╲
+/// //    ╱   ╲
+/// //   2     3
+/// //  ╱ ╲   ╱ ╲
+/// // 4   5 6   7
+/// // |     |  ╱ ╲
+/// // 8     9 10  11
+/// let mut tree = BinaryTree::<i32>::new(1);
+///
+/// let mut root = tree.root_mut().unwrap();
+/// root.extend([2, 3]);
+///
+/// let mut n2 = root.child_mut(0).unwrap();
+/// n2.extend([4, 5]);
+///
+/// let mut n4 = n2.child_mut(0).unwrap();
+/// n4.push(8);
+///
+/// let mut n3 = tree.root_mut().unwrap().child_mut(1).unwrap();
+/// n3.extend([6, 7]);
+///
+/// let mut n6 = n3.child_mut(0).unwrap();
+/// n6.push(9);
+///
+/// let mut n7 = n6.parent_mut().unwrap().child_mut(1).unwrap();
+/// n7.extend([10, 11]);
+///
+/// // dfs over (depth, sibling index, data)
+///
+/// let root = tree.root().unwrap();
+///
+/// for data in root.dfs_over::<OverData>() {
+///     // ...
+/// }
+///
+/// let mut iter = root.dfs_over::<OverData>();
+/// assert_eq!(iter.next(), Some(&1));
+/// assert_eq!(iter.next(), Some(&2));
+/// assert_eq!(iter.next(), Some(&4));
+/// assert_eq!(iter.next(), Some(&8));
+/// assert_eq!(iter.next(), Some(&5)); // ...
+///
+/// let values: Vec<i32> = root.dfs_over::<OverData>().copied().collect();
+/// assert_eq!(values, [1, 2, 4, 8, 5, 3, 6, 9, 7, 10, 11]);
+///
+/// // dfs over (depth, sibling index, node)
+///
+/// for node in root.dfs_over::<OverNode>() {
+///     // ...
+/// }
+///
+/// let nodes: Vec<Node<_>> = root.dfs_over::<OverNode>().collect();
+///
+/// let values: Vec<i32> = nodes.iter().map(|x| *x.data()).collect();
+/// assert_eq!(values, [1, 2, 4, 8, 5, 3, 6, 9, 7, 10, 11]);
+///
+/// let num_children: Vec<usize> = nodes.iter().map(|x| x.num_children()).collect();
+/// assert_eq!(num_children, [2, 2, 1, 0, 0, 2, 1, 0, 2, 0, 0]);
+/// ```
 pub struct OverData;
 
 impl IterOver for OverData {
@@ -51,6 +120,74 @@ impl IterOver for OverData {
         P: PinnedVec<N<V>> + 'a;
 }
 
+/// Iterator directly over tree nodes.
+///
+/// # Examples
+///
+/// ```
+/// use orx_tree::*;
+/// use orx_tree::iter::*;
+///
+/// //      1
+/// //     ╱ ╲
+/// //    ╱   ╲
+/// //   2     3
+/// //  ╱ ╲   ╱ ╲
+/// // 4   5 6   7
+/// // |     |  ╱ ╲
+/// // 8     9 10  11
+/// let mut tree = BinaryTree::<i32>::new(1);
+///
+/// let mut root = tree.root_mut().unwrap();
+/// root.extend([2, 3]);
+///
+/// let mut n2 = root.child_mut(0).unwrap();
+/// n2.extend([4, 5]);
+///
+/// let mut n4 = n2.child_mut(0).unwrap();
+/// n4.push(8);
+///
+/// let mut n3 = tree.root_mut().unwrap().child_mut(1).unwrap();
+/// n3.extend([6, 7]);
+///
+/// let mut n6 = n3.child_mut(0).unwrap();
+/// n6.push(9);
+///
+/// let mut n7 = n6.parent_mut().unwrap().child_mut(1).unwrap();
+/// n7.extend([10, 11]);
+///
+/// // dfs over (depth, sibling index, data)
+///
+/// let root = tree.root().unwrap();
+///
+/// for data in root.dfs_over::<OverData>() {
+///     // ...
+/// }
+///
+/// let mut iter = root.dfs_over::<OverData>();
+/// assert_eq!(iter.next(), Some(&1));
+/// assert_eq!(iter.next(), Some(&2));
+/// assert_eq!(iter.next(), Some(&4));
+/// assert_eq!(iter.next(), Some(&8));
+/// assert_eq!(iter.next(), Some(&5)); // ...
+///
+/// let values: Vec<i32> = root.dfs_over::<OverData>().copied().collect();
+/// assert_eq!(values, [1, 2, 4, 8, 5, 3, 6, 9, 7, 10, 11]);
+///
+/// // dfs over (depth, sibling index, node)
+///
+/// for node in root.dfs_over::<OverNode>() {
+///     // ...
+/// }
+///
+/// let nodes: Vec<Node<_>> = root.dfs_over::<OverNode>().collect();
+///
+/// let values: Vec<i32> = nodes.iter().map(|x| *x.data()).collect();
+/// assert_eq!(values, [1, 2, 4, 8, 5, 3, 6, 9, 7, 10, 11]);
+///
+/// let num_children: Vec<usize> = nodes.iter().map(|x| x.num_children()).collect();
+/// assert_eq!(num_children, [2, 2, 1, 0, 0, 2, 1, 0, 2, 0, 0]);
+/// ```
 pub struct OverNode;
 
 impl IterOver for OverNode {
--- src/iter/iter_kinds/depth_data.rs
+++ src/iter/iter_kinds/depth_data.rs
@@ -9,6 +9,7 @@ use orx_selfref_col::{MemoryPolicy, NodePtr, SelfRefCol};
 
 // core
 
+/// Iterator over tuples of node depths and values obtained from tree nodes.
 pub struct NodeDepthVal<D>(PhantomData<D>);
 
 impl<'a, V, M, P, D> IterKindCore<'a, V, M, P> for NodeDepthVal<D>
@@ -48,6 +49,78 @@ where
 
 // over
 
+/// Iterator over (node depth, node data) pairs.
+///
+/// # Examples
+///
+/// ```
+/// use orx_tree::*;
+/// use orx_tree::iter::*;
+///
+/// //      1
+/// //     ╱ ╲
+/// //    ╱   ╲
+/// //   2     3
+/// //  ╱ ╲   ╱ ╲
+/// // 4   5 6   7
+/// // |     |  ╱ ╲
+/// // 8     9 10  11
+/// let mut tree = BinaryTree::<i32>::new(1);
+///
+/// let mut root = tree.root_mut().unwrap();
+/// root.extend([2, 3]);
+///
+/// let mut n2 = root.child_mut(0).unwrap();
+/// n2.extend([4, 5]);
+///
+/// let mut n4 = n2.child_mut(0).unwrap();
+/// n4.push(8);
+///
+/// let mut n3 = tree.root_mut().unwrap().child_mut(1).unwrap();
+/// n3.extend([6, 7]);
+///
+/// let mut n6 = n3.child_mut(0).unwrap();
+/// n6.push(9);
+///
+/// let mut n7 = n6.parent_mut().unwrap().child_mut(1).unwrap();
+/// n7.extend([10, 11]);
+///
+/// // dfs over (depth, data)
+///
+/// let root = tree.root().unwrap();
+///
+/// for (depth, data) in root.dfs_over::<OverDepthData>() {
+///     // ...
+/// }
+///
+/// let mut iter = root.dfs_over::<OverDepthData>();
+/// assert_eq!(iter.next(), Some((0, &1)));
+/// assert_eq!(iter.next(), Some((1, &2)));
+/// assert_eq!(iter.next(), Some((2, &4)));
+/// assert_eq!(iter.next(), Some((3, &8)));
+/// assert_eq!(iter.next(), Some((2, &5))); // ...
+///
+/// let all: Vec<(usize, &i32)> = root.dfs_over::<OverDepthData>().collect();
+///
+/// let depths: Vec<usize> = all.iter().map(|x| x.0).collect();
+/// assert_eq!(depths, [0, 1, 2, 3, 2, 1, 2, 3, 2, 3, 3]);
+///
+/// let values: Vec<i32> = all.iter().map(|x| *x.1).collect();
+/// assert_eq!(values, [1, 2, 4, 8, 5, 3, 6, 9, 7, 10, 11]);
+///
+/// // dfs over (depth, node)
+///
+/// let all: Vec<(usize, Node<_>)> = root.dfs_over::<OverDepthNode>().collect();
+///
+/// let depths: Vec<usize> = all.iter().map(|x| x.0).collect();
+/// assert_eq!(depths, [0, 1, 2, 3, 2, 1, 2, 3, 2, 3, 3]);
+///
+/// let values: Vec<i32> = all.iter().map(|x| *x.1.data()).collect();
+/// assert_eq!(values, [1, 2, 4, 8, 5, 3, 6, 9, 7, 10, 11]);
+///
+/// let num_children: Vec<usize> = all.iter().map(|x| x.1.num_children()).collect();
+/// assert_eq!(num_children, [2, 2, 1, 0, 0, 2, 1, 0, 2, 0, 0]);
+/// ```
 pub struct OverDepthData;
 
 impl IterOver for OverDepthData {
@@ -59,6 +132,78 @@ impl IterOver for OverDepthData {
         P: PinnedVec<N<V>> + 'a;
 }
 
+/// Iterator over (node depth, node) pairs.
+///
+/// # Examples
+///
+/// ```
+/// use orx_tree::*;
+/// use orx_tree::iter::*;
+///
+/// //      1
+/// //     ╱ ╲
+/// //    ╱   ╲
+/// //   2     3
+/// //  ╱ ╲   ╱ ╲
+/// // 4   5 6   7
+/// // |     |  ╱ ╲
+/// // 8     9 10  11
+/// let mut tree = BinaryTree::<i32>::new(1);
+///
+/// let mut root = tree.root_mut().unwrap();
+/// root.extend([2, 3]);
+///
+/// let mut n2 = root.child_mut(0).unwrap();
+/// n2.extend([4, 5]);
+///
+/// let mut n4 = n2.child_mut(0).unwrap();
+/// n4.push(8);
+///
+/// let mut n3 = tree.root_mut().unwrap().child_mut(1).unwrap();
+/// n3.extend([6, 7]);
+///
+/// let mut n6 = n3.child_mut(0).unwrap();
+/// n6.push(9);
+///
+/// let mut n7 = n6.parent_mut().unwrap().child_mut(1).unwrap();
+/// n7.extend([10, 11]);
+///
+/// // dfs over (depth, data)
+///
+/// let root = tree.root().unwrap();
+///
+/// for (depth, data) in root.dfs_over::<OverDepthData>() {
+///     // ...
+/// }
+///
+/// let mut iter = root.dfs_over::<OverDepthData>();
+/// assert_eq!(iter.next(), Some((0, &1)));
+/// assert_eq!(iter.next(), Some((1, &2)));
+/// assert_eq!(iter.next(), Some((2, &4)));
+/// assert_eq!(iter.next(), Some((3, &8)));
+/// assert_eq!(iter.next(), Some((2, &5))); // ...
+///
+/// let all: Vec<(usize, &i32)> = root.dfs_over::<OverDepthData>().collect();
+///
+/// let depths: Vec<usize> = all.iter().map(|x| x.0).collect();
+/// assert_eq!(depths, [0, 1, 2, 3, 2, 1, 2, 3, 2, 3, 3]);
+///
+/// let values: Vec<i32> = all.iter().map(|x| *x.1).collect();
+/// assert_eq!(values, [1, 2, 4, 8, 5, 3, 6, 9, 7, 10, 11]);
+///
+/// // dfs over (depth, node)
+///
+/// let all: Vec<(usize, Node<_>)> = root.dfs_over::<OverDepthNode>().collect();
+///
+/// let depths: Vec<usize> = all.iter().map(|x| x.0).collect();
+/// assert_eq!(depths, [0, 1, 2, 3, 2, 1, 2, 3, 2, 3, 3]);
+///
+/// let values: Vec<i32> = all.iter().map(|x| *x.1.data()).collect();
+/// assert_eq!(values, [1, 2, 4, 8, 5, 3, 6, 9, 7, 10, 11]);
+///
+/// let num_children: Vec<usize> = all.iter().map(|x| x.1.num_children()).collect();
+/// assert_eq!(num_children, [2, 2, 1, 0, 0, 2, 1, 0, 2, 0, 0]);
+/// ```
 pub struct OverDepthNode;
 
 impl IterOver for OverDepthNode {
