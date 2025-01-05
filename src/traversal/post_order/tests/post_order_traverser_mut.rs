3e13503404bca85bfdad3946cb2dda9034c108ad
orxfun <orx.ugur.arikan@gmail.com>
2025-01-05 20:26:47 +0100

    TraverserMut tests for post-order

---
+++ src/traversal/post_order/tests/post_order_traverser_mut.rs
@@ -0,0 +1,116 @@
+use crate::{
+    traversal::{
+        over::{OverData, OverDepthData, OverDepthSiblingIdxData, OverSiblingIdxData},
+        post_order::traverser::PostOrder,
+        traverser_mut::TraverserMut,
+        Traverser,
+    },
+    AsTreeNode, Dyn, DynTree,
+};
+use alloc::vec::Vec;
+
+/// ```
+///      1
+///     ╱ ╲
+///    ╱   ╲
+///   2     3
+///  ╱ ╲   ╱ ╲
+/// 4   5 6   7
+/// |     |  ╱ ╲
+/// 8     9 10  11
+/// ```
+fn tree() -> DynTree<i32> {
+    let mut tree = DynTree::<i32>::new(1);
+
+    let mut root = tree.root_mut().unwrap();
+    let [id2, id3] = root.grow([2, 3]);
+
+    let mut n2 = id2.node_mut(&mut tree);
+    let [id4, _] = n2.grow([4, 5]);
+
+    id4.node_mut(&mut tree).push(8);
+
+    let mut n3 = id3.node_mut(&mut tree);
+    let [id6, id7] = n3.grow([6, 7]);
+
+    id6.node_mut(&mut tree).push(9);
+    id7.node_mut(&mut tree).extend([10, 11]);
+
+    tree
+}
+
+#[test]
+fn dfs_iter_mut_val() {
+    let mut tree = tree();
+    let mut traverser = PostOrder::<Dyn<i32>, OverData>::default();
+
+    let mut root = tree.root_mut().unwrap();
+    let iter = traverser.iter_mut(&mut root);
+    for (i, x) in iter.enumerate() {
+        *x += 100 * i as i32;
+    }
+
+    let root = tree.root().unwrap();
+    let iter = traverser.iter(&root);
+    assert_eq!(
+        iter.copied().collect::<Vec<_>>(),
+        [8, 104, 205, 302, 409, 506, 610, 711, 807, 903, 1001]
+    );
+}
+
+#[test]
+fn dfs_iter_mut_depth() {
+    let mut tree = tree();
+    let mut traverser = PostOrder::<Dyn<i32>, OverDepthData>::default();
+
+    let mut root = tree.root_mut().unwrap();
+    let iter = traverser.iter_mut(&mut root);
+    for (d, x) in iter {
+        *x += 100 * d as i32;
+    }
+
+    let root = tree.root().unwrap();
+    let iter = traverser.iter(&root);
+    assert_eq!(
+        iter.map(|x| *x.1).collect::<Vec<_>>(),
+        [308, 204, 205, 102, 309, 206, 310, 311, 207, 103, 1]
+    );
+}
+
+#[test]
+fn dfs_iter_mut_sibling() {
+    let mut tree = tree();
+    let mut traverser = PostOrder::<Dyn<i32>, OverSiblingIdxData>::default();
+
+    let mut root = tree.root_mut().unwrap();
+    let iter = traverser.iter_mut(&mut root);
+    for (s, x) in iter {
+        *x += 100 * s as i32;
+    }
+
+    let root = tree.root().unwrap();
+    let iter = traverser.iter(&root);
+    assert_eq!(
+        iter.map(|x| *x.1).collect::<Vec<_>>(),
+        [8, 4, 105, 2, 9, 6, 10, 111, 107, 103, 1]
+    );
+}
+
+#[test]
+fn dfs_iter_mut_depth_sibling() {
+    let mut tree = tree();
+    let mut traverser = PostOrder::<Dyn<i32>, OverDepthSiblingIdxData>::default();
+
+    let mut root = tree.root_mut().unwrap();
+    let iter = traverser.iter_mut(&mut root);
+    for (d, s, x) in iter {
+        *x += 10000 * d as i32 + 100 * s as i32;
+    }
+
+    let root = tree.root().unwrap();
+    let iter = traverser.iter(&root);
+    assert_eq!(
+        iter.map(|x| *x.2).collect::<Vec<_>>(),
+        [30008, 20004, 20105, 10002, 30009, 20006, 30010, 30111, 20107, 10103, 1]
+    );
+}
