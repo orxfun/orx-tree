915289c23c5e4e7ba697ec89d6da3156b07d5f93
orxfun <orx.ugur.arikan@gmail.com>
2025-01-05 18:07:15 +0100

    traverser trait is defined

--- src/traversal/traverser.rs
+++ src/traversal/traverser.rs
@@ -1,30 +1,44 @@
 use super::{element::Element, node_item::NodeItem, node_item_mut::NodeItemMut};
-use crate::{helpers::N, Node, NodeMut, NodeRef, TreeVariant};
+use crate::{helpers::N, NodeMut, NodeRef, TreeVariant};
 use orx_pinned_vec::PinnedVec;
-use orx_selfref_col::{MemoryPolicy, NodePtr, SelfRefCol};
+use orx_selfref_col::MemoryPolicy;
 
-pub trait Traverser {
+pub trait Traverser<V: TreeVariant> {
     type ItemKind: Element;
 
-    // type NodeItem: NodeItem;
+    type NodeItem<'a, M, P>: NodeItem<'a, V, M, P>
+    where
+        M: MemoryPolicy<V> + 'a,
+        P: PinnedVec<N<V>> + 'a,
+        V: 'a,
+        Self: 'a;
 
-    // type NodeItemMut: NodeItemMut;
+    fn iter<'a, M, P>(
+        &mut self,
+        node: &impl NodeRef<'a, V, M, P>,
+    ) -> impl Iterator<Item = <Self::ItemKind as Element>::Item<Self::NodeItem<'a, M, P>>>
+    where
+        V: TreeVariant + 'a,
+        M: MemoryPolicy<V> + 'a,
+        P: PinnedVec<N<V>> + 'a,
+        Self: 'a;
+}
 
-    // fn iter<'a, V, M, P>(
-    //     &mut self,
-    //     node: &impl NodeRef<'a, V, M, P>,
-    // ) -> impl Iterator<Item = <Self::ItemKind as Element>::Item<Self::NodeData>>
-    // where
-    //     V: TreeVariant + 'a,
-    //     M: MemoryPolicy<V> + 'a,
-    //     P: PinnedVec<N<V>> + 'a;
+pub trait TraverserMut<V: TreeVariant>: Traverser<V> {
+    type NodeItemMut<'a, M, P>: NodeItemMut<'a, V, M, P>
+    where
+        M: MemoryPolicy<V> + 'a,
+        P: PinnedVec<N<V>> + 'a,
+        V: 'a,
+        Self: 'a;
 
-    // fn iter_mut<'a, V, M, P>(
-    //     &mut self,
-    //     node_mut: &mut NodeMut<'a, V, M, P>,
-    // ) -> impl Iterator<Item = <Self::ItemKind as Element>::Item<Self::NodeDataMut>>
-    // where
-    //     V: TreeVariant + 'a,
-    //     M: MemoryPolicy<V> + 'a,
-    //     P: PinnedVec<N<V>> + 'a;
+    fn iter_mut<'a, M, P>(
+        &mut self,
+        node_mut: &mut NodeMut<'a, V, M, P>,
+    ) -> impl Iterator<Item = <Self::ItemKind as Element>::Item<Self::NodeItemMut<'a, M, P>>>
+    where
+        V: TreeVariant + 'a,
+        M: MemoryPolicy<V> + 'a,
+        P: PinnedVec<N<V>> + 'a,
+        Self: 'a;
 }
