c412d298244081c68a6dcc3760cb4f82c9d1b735
orxfun <orx.ugur.arikan@gmail.com>
2025-01-06 22:41:42 +0100

    all iterators of breadth first search are tested

---
+++ src/traversal/breadth_first/tests/bfs_iter_mut.rs
@@ -0,0 +1,143 @@
+use crate::{
+    node_ref::NodeRefCore,
+    traversal::breadth_first::{iter_mut::BfsIterMut, iter_ptr::BfsIterPtr, iter_ref::BfsIterRef},
+    traversal::enumerations::{DepthSiblingIdxVal, DepthVal, SiblingIdxVal, Val},
+    AsTreeNode, Dyn, DynTree,
+};
+use alloc::collections::VecDeque;
+use alloc::vec::Vec;
+
+/// ```
+///       1
+///     ╱ ╲
+///    ╱   ╲
+///   2     3
+///  ╱ ╲   ╱ ╲
+/// 4   5 6   7
+/// |     |  ╱ ╲
+/// 8     9 10  11
+/// ```
+fn tree() -> DynTree<i32> {
+    let mut tree = DynTree::<i32>::new(1);
+
+    let mut root = tree.root_mut().unwrap();
+    let [id2, id3] = root.grow([2, 3]);
+
+    let mut n2 = id2.node_mut(&mut tree);
+    let [id4, _] = n2.grow([4, 5]);
+
+    id4.node_mut(&mut tree).push(8);
+
+    let mut n3 = id3.node_mut(&mut tree);
+    let [id6, id7] = n3.grow([6, 7]);
+
+    id6.node_mut(&mut tree).push(9);
+    id7.node_mut(&mut tree).extend([10, 11]);
+
+    tree
+}
+
+#[test]
+fn bfs_iter_ref_empty() {
+    let tree = DynTree::<i32>::empty();
+    let iter = BfsIterPtr::<Dyn<i32>, Val>::default();
+    let mut iter = unsafe { BfsIterMut::<_, _, _, Val, _, &mut i32>::from((&tree.0, iter)) };
+    assert_eq!(iter.next(), None);
+}
+
+#[test]
+fn bfs_iter_mut_val() {
+    let tree = tree();
+    let mut queue = VecDeque::default();
+
+    let root = tree.root().unwrap();
+    let ptr = root.node_ptr().clone();
+    let iter = BfsIterPtr::<_, Val, _>::from((&mut queue, ptr));
+    let iter = unsafe { BfsIterMut::<_, _, _, Val, _, &mut i32>::from((&tree.0, iter)) };
+
+    for (i, x) in iter.enumerate() {
+        *x += 100 * i as i32;
+    }
+
+    let root = tree.root().unwrap();
+    let ptr = root.node_ptr().clone();
+    let iter = BfsIterPtr::<_, Val, _>::from((&mut queue, ptr));
+    let iter = BfsIterRef::<_, _, _, Val, _, &i32>::from((root.col(), iter));
+    assert_eq!(
+        iter.copied().collect::<Vec<_>>(),
+        [1, 102, 203, 304, 405, 506, 607, 708, 809, 910, 1011]
+    );
+}
+
+#[test]
+fn bfs_iter_mut_depth() {
+    let tree = tree();
+    let mut queue = VecDeque::default();
+
+    let root = tree.root().unwrap();
+    let ptr = root.node_ptr().clone();
+    let iter = BfsIterPtr::<_, DepthVal, _>::from((&mut queue, ptr));
+    let iter = unsafe { BfsIterMut::<_, _, _, DepthVal, _, &mut i32>::from((&tree.0, iter)) };
+
+    for (d, x) in iter {
+        *x += 100 * d as i32;
+    }
+
+    let root = tree.root().unwrap();
+    let ptr = root.node_ptr().clone();
+    let iter = BfsIterPtr::<_, Val, _>::from((VecDeque::new(), ptr));
+    let iter = BfsIterRef::<_, _, _, Val, _, &i32>::from((root.col(), iter));
+    assert_eq!(
+        iter.copied().collect::<Vec<_>>(),
+        [1, 102, 103, 204, 205, 206, 207, 308, 309, 310, 311]
+    );
+}
+
+#[test]
+fn bfs_iter_mut_sibling() {
+    let tree = tree();
+    let mut queue = VecDeque::default();
+
+    let root = tree.root().unwrap();
+    let ptr = root.node_ptr().clone();
+    let iter = BfsIterPtr::<_, SiblingIdxVal, _>::from((&mut queue, ptr));
+    let iter = unsafe { BfsIterMut::<_, _, _, SiblingIdxVal, _, &mut i32>::from((&tree.0, iter)) };
+
+    for (s, x) in iter {
+        *x += 100 * s as i32;
+    }
+
+    let root = tree.root().unwrap();
+    let ptr = root.node_ptr().clone();
+    let iter = BfsIterPtr::<_, Val, _>::from((VecDeque::new(), ptr));
+    let iter = BfsIterRef::<_, _, _, Val, _, &i32>::from((root.col(), iter));
+    assert_eq!(
+        iter.copied().collect::<Vec<_>>(),
+        [1, 2, 103, 4, 105, 6, 107, 8, 9, 10, 111]
+    );
+}
+
+#[test]
+fn bfs_iter_mut_depth_sibling() {
+    let tree = tree();
+    let mut queue = VecDeque::default();
+
+    let root = tree.root().unwrap();
+    let ptr = root.node_ptr().clone();
+    let iter = BfsIterPtr::<_, DepthSiblingIdxVal, _>::from((&mut queue, ptr));
+    let iter =
+        unsafe { BfsIterMut::<_, _, _, DepthSiblingIdxVal, _, &mut i32>::from((&tree.0, iter)) };
+
+    for (d, s, x) in iter {
+        *x += 10000 * d as i32 + 100 * s as i32;
+    }
+
+    let root = tree.root().unwrap();
+    let ptr = root.node_ptr().clone();
+    let iter = BfsIterPtr::<_, Val, _>::from((VecDeque::new(), ptr));
+    let iter = BfsIterRef::<_, _, _, Val, _, &i32>::from((root.col(), iter));
+    assert_eq!(
+        iter.copied().collect::<Vec<_>>(),
+        [1, 10002, 10103, 20004, 20105, 20006, 20107, 30008, 30009, 30010, 30111]
+    );
+}
