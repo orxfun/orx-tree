99b41447dac535618fb989ebd50e4273a43693b4
orxfun <orx.ugur.arikan@gmail.com>
2025-01-10 14:16:50 +0100

    root sibling position returns 0 rather than None

--- src/common_traits/debug.rs
+++ src/common_traits/debug.rs
@@ -1,4 +1,6 @@
-use crate::{memory::MemoryPolicy, pinned_storage::PinnedStorage, Node, NodeMut, TreeVariant};
+use crate::{
+    memory::MemoryPolicy, pinned_storage::PinnedStorage, Node, NodeMut, NodeRef, TreeVariant,
+};
 use core::fmt::Debug;
 
 impl<V, M, P> Debug for Node<'_, V, M, P>
@@ -6,10 +8,10 @@ where
     V: TreeVariant,
     M: MemoryPolicy,
     P: PinnedStorage,
+    V::Item: Debug,
 {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        // TODO
-        f.debug_struct("Node").finish()
+        fmt_node(f, self)
     }
 }
 
@@ -18,9 +20,28 @@ where
     V: TreeVariant,
     M: MemoryPolicy,
     P: PinnedStorage,
+    V::Item: Debug,
 {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        // TODO
-        f.debug_struct("NodeMut").finish()
+        fmt_node(f, self)
     }
+}
+
+fn fmt_node<'a, V, M, P>(
+    f: &mut core::fmt::Formatter<'_>,
+    node: &impl NodeRef<'a, V, M, P>,
+) -> core::fmt::Result
+where
+    V: TreeVariant + 'a,
+    M: MemoryPolicy,
+    P: PinnedStorage,
+    V::Item: Debug,
+{
+    // TODO: additional info, such as depth
+    f.debug_struct("Node")
+        .field("is_root", &node.is_root())
+        .field("sibling_position", &node.sibling_position())
+        .field("num_children", &node.num_children())
+        .field("data", node.data())
+        .finish()
 }
