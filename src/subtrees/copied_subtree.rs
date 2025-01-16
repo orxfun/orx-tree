367b678f26d4aa39aec825cf018c25e9857be4c2
orxfun <orx.ugur.arikan@gmail.com>
2025-01-16 23:53:34 +0100

    NodeRef and NodeMut as subtree

---
+++ src/subtrees/copied_subtree.rs
@@ -0,0 +1,66 @@
+use super::SubTree;
+use crate::{
+    pinned_storage::PinnedStorage,
+    traversal::{over::OverDepthPtr, traverser_core::TraverserCore},
+    Dfs, MemoryPolicy, NodeIdx, NodeMut, NodeMutOrientation, NodeRef, TreeVariant,
+};
+use core::marker::PhantomData;
+
+pub struct CopiedSubTree<'a, V, M, P, N>
+where
+    V: TreeVariant + 'a,
+    M: MemoryPolicy,
+    P: PinnedStorage,
+    N: NodeRef<'a, V, M, P>,
+    V::Item: Copy,
+{
+    node: N,
+    phantom: PhantomData<&'a (V, M, P)>,
+}
+
+impl<'a, V, M, P, N> CopiedSubTree<'a, V, M, P, N>
+where
+    V: TreeVariant + 'a,
+    M: MemoryPolicy,
+    P: PinnedStorage,
+    N: NodeRef<'a, V, M, P>,
+    V::Item: Copy,
+{
+    pub(crate) fn new(node: N) -> Self {
+        Self {
+            node,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a, V, M, P, N> SubTree<V::Item> for CopiedSubTree<'a, V, M, P, N>
+where
+    V: TreeVariant + 'a,
+    M: MemoryPolicy,
+    P: PinnedStorage,
+    N: NodeRef<'a, V, M, P>,
+    V::Item: Copy,
+{
+    fn append_to_node_as_child<V2, M2, P2, MO>(
+        self,
+        parent: &mut NodeMut<V2, M2, P2, MO>,
+    ) -> NodeIdx<V2>
+    where
+        V2: TreeVariant<Item = V::Item>,
+        M2: MemoryPolicy,
+        P2: PinnedStorage,
+        MO: NodeMutOrientation,
+    {
+        let ptr = self.node.node_ptr().clone();
+        let iter_ptr = Dfs::<OverDepthPtr>::iter_ptr_with_owned_storage(ptr);
+        let subtree = iter_ptr.map(|(depth, ptr)| {
+            (
+                depth,
+                *unsafe { &*ptr.ptr() }.data().expect("node is active"),
+            )
+        });
+
+        parent.append_subtree_as_child(subtree)
+    }
+}
--- src/subtrees/node_mut_as_subtree.rs
+++ src/subtrees/node_mut_as_subtree.rs
@@ -1,27 +1,50 @@
 use super::subtree::SubTree;
 use crate::{
     pinned_storage::PinnedStorage,
-    traversal::{over::OverDepthPtr, traverser_core::TraverserCore},
-    Dfs, MemoryPolicy, NodeIdx, NodeMut, TreeVariant,
+    traversal::{traverser_core::TraverserCore, OverDepthData},
+    Dfs, MemoryPolicy, NodeIdx, NodeMut, NodeMutOrientation, TreeVariant,
 };
 
-impl<'a, V, M, P> SubTree<V::Item> for NodeMut<'a, V, M, P>
+pub struct NodeMutAsSubTree<'a, V, M, P, MO>
 where
-    V: TreeVariant,
+    V: TreeVariant + 'a,
     M: MemoryPolicy,
     P: PinnedStorage,
+    MO: NodeMutOrientation,
 {
-    fn append_to_node_as_child<V2, M2, P2, MO>(
+    node: NodeMut<'a, V, M, P, MO>,
+}
+
+impl<'a, V, M, P, MO> NodeMutAsSubTree<'a, V, M, P, MO>
+where
+    V: TreeVariant + 'a,
+    M: MemoryPolicy,
+    P: PinnedStorage,
+    MO: NodeMutOrientation,
+{
+    pub(crate) fn new(node: NodeMut<'a, V, M, P, MO>) -> Self {
+        Self { node }
+    }
+}
+
+impl<'a, V, M, P, MO> SubTree<V::Item> for NodeMutAsSubTree<'a, V, M, P, MO>
+where
+    V: TreeVariant + 'a,
+    M: MemoryPolicy,
+    P: PinnedStorage,
+    MO: NodeMutOrientation,
+{
+    fn append_to_node_as_child<V2, M2, P2, MO2>(
         self,
-        parent: &mut NodeMut<V2, M2, P2, MO>,
+        parent: &mut NodeMut<V2, M2, P2, MO2>,
     ) -> NodeIdx<V2>
     where
         V2: TreeVariant<Item = V::Item>,
         M2: MemoryPolicy,
         P2: PinnedStorage,
-        MO: crate::NodeMutOrientation,
+        MO2: NodeMutOrientation,
     {
-        let subtree = Dfs::<OverDepthPtr>::into_iter_with_owned_storage(self);
+        let subtree = Dfs::<OverDepthData>::into_iter_with_owned_storage(self.node);
         parent.append_subtree_as_child(subtree)
     }
 }
