0cfb79d1a9182079b1cdd9222c8f927b13961e22
orxfun <orx.ugur.arikan@gmail.com>
2025-11-26 16:57:50 +0100

    root_into_new_tree test

--- tests/leaf_nodes.rs
+++ tests/leaf_nodes.rs
@@ -2,14 +2,25 @@
 
 use orx_tree::*;
 
+#[test]
+fn root_into_new_tree() {
+    let mut tree = DynTree::new(0);
+
+    let tree2: DynTree<_> = tree.root_mut().into_new_tree();
+
+    assert_eq!(tree2.len(), 1);
+    assert_eq!(
+        tree2.root().walk::<Dfs>().copied().collect::<Vec<_>>(),
+        vec![0]
+    );
+}
+
 /// https://github.com/orxfun/orx-tree/issues/183
 #[test]
 fn leaf_into_new_tree() {
     let mut tree = DynTree::new(0);
     tree.root_mut().push_child(1);
 
-    // Does not panic if we add a children to 1
-    // tree.root_mut().children_mut().next().unwrap().push_child(2);
     let tree2: DynTree<_> = tree
         .root_mut()
         .children_mut()
