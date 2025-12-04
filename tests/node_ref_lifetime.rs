3c25ec61f973c27cf30f629cd914dde5770da7b0
orxfun <orx.ugur.arikan@gmail.com>
2025-12-04 18:32:50 +0100

    initiate test for walk with

---
+++ tests/node_ref_lifetime.rs
@@ -0,0 +1,21 @@
+use orx_tree::traversal::*;
+use orx_tree::*;
+
+fn find_node<'a, V: TreeVariant>(tree: &'a Tree<V>, predicate: &V::Item) -> Option<Node<'a, V>>
+where
+    V::Item: Eq,
+{
+    let mut traverser = Dfs::<OverNode>::new();
+    let root_node = tree.get_root().unwrap();
+    let node = {
+        let mut walker = root_node.walk_with(&mut traverser);
+        let x = walker.find(|v| v.data() == predicate);
+        // drop(walker);
+        // drop(traverser);
+        x
+    };
+
+    // ERROR: cannot return value referencing local variable `traverser`
+    // return node;
+    None
+}
