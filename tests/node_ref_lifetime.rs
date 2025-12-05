340f7f4cc464f9c09011d27487558488e856f657
orxfun <orx.ugur.arikan@gmail.com>
2025-12-05 10:39:05 +0100

    add find_walk_par lifetime test

--- tests/node_ref_lifetime.rs
+++ tests/node_ref_lifetime.rs
@@ -5,13 +5,17 @@ use orx_tree::*;
 These tests are created wrt the issue: https://github.com/orxfun/orx-tree/issues/188.
 
 They are expected to compile.
+
+The `find` methods below return a node referencing the `tree`, which must be independent
+of the root node or traverser if provided externally.
+
+The methods must NOT fail to compile due to the following errors:
+* referencing local variable `traverser`, or
+* referencing local variable `root_node`.
+
+These are temporary references. The output's lifetime must not depend on these temporary values.
 */
 
-/// This method returns a Node that is referencing the `tree`.
-///
-/// It must not fail to compile by due to errors:
-/// * referencing local variable `traverser`, or
-/// * referencing local variable `root_node`.
 fn find_walk<'a, V: TreeVariant>(tree: &'a Tree<V>, predicate: &V::Item) -> Option<&'a V::Item>
 where
     V::Item: Eq,
@@ -21,11 +25,15 @@ where
     walker.find(|v| v == &predicate)
 }
 
-/// This method returns a Node that is referencing the `tree`.
-///
-/// It must not fail to compile by due to errors:
-/// * referencing local variable `traverser`, or
-/// * referencing local variable `root_node`.
+fn find_walk_par<'a, V: TreeVariant>(tree: &'a Tree<V>, predicate: &V::Item) -> Option<&'a V::Item>
+where
+    V::Item: Eq + Sync + Send,
+{
+    let root_node = tree.get_root()?;
+    let walker = root_node.walk_par::<Dfs>();
+    walker.find(|v| v == &predicate)
+}
+
 fn find_walk_with<'a, V: TreeVariant>(tree: &'a Tree<V>, predicate: &V::Item) -> Option<Node<'a, V>>
 where
     V::Item: Eq,
@@ -41,6 +49,9 @@ fn node_ref_lifetime_tests() {
     let tree = DynTree::new(42);
 
     let node = find_walk(&tree, &33);
+    assert_eq!(node, None);
+
+    let node = find_walk_par(&tree, &33);
     assert_eq!(node, None);
 
     let node = find_walk_with(&tree, &33);
