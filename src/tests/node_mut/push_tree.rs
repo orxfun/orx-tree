c5929822f89a6e299acb70b23a280fa89f679f62
orxfun <orx.ugur.arikan@gmail.com>
2026-01-01 13:10:03 +0100

    add push_child_tree_within_copied tests

--- src/tests/node_mut/push_tree.rs
+++ src/tests/node_mut/push_tree.rs
@@ -1,6 +1,5 @@
 use crate::*;
 use std::{
-    dbg, println,
     string::{String, ToString},
     vec::Vec,
 };
@@ -8,13 +7,21 @@ use std::{
 fn to_str<const N: usize>(values: [i32; N]) -> [String; N] {
     values.map(|x| x.to_string())
 }
-
 fn get_main_tree() -> DynTree<String> {
     let mut tree = DynTree::new(0.to_string());
     let [id1, id2] = tree.root_mut().push_children(to_str([1, 2]));
     let [_id3, _id4, _id5] = tree.node_mut(id1).push_children(to_str([3, 4, 5]));
     let [id6] = tree.node_mut(id2).push_children(to_str([6]));
     let [_id7, _id8] = tree.node_mut(id6).push_children(to_str([7, 8]));
+    tree
+}
+
+fn get_main_tree_copy() -> DynTree<i32> {
+    let mut tree = DynTree::new(0);
+    let [id1, id2] = tree.root_mut().push_children([1, 2]);
+    let [_id3, _id4, _id5] = tree.node_mut(id1).push_children([3, 4, 5]);
+    let [id6] = tree.node_mut(id2).push_children([6]);
+    let [_id7, _id8] = tree.node_mut(id6).push_children([7, 8]);
     tree
 }
 
@@ -107,6 +114,35 @@ fn push_child_tree_within_cloned() {
             tree.node_mut(id_dst).push_child_tree_within(subtree);
 
             let mut nodes: Vec<_> = tree.root().walk::<Bfs>().cloned().collect();
+            nodes.sort();
+
+            assert_eq!(nodes, expected_nodes);
+        }
+    }
+}
+
+/// Subtrees can be pushed as child trees of any node when cloned,
+/// no potential panic.
+#[test]
+fn push_child_tree_within_copied() {
+    let tree = get_main_tree_copy();
+
+    let initial_nodes: Vec<_> = tree.root().walk::<Bfs>().copied().collect();
+
+    for i in 0..tree.len() {
+        for j in 0..tree.len() {
+            let mut tree = tree.clone();
+            let id_dst = tree.root().indices::<Bfs>().nth(i).unwrap();
+            let id_src = tree.root().indices::<Bfs>().nth(j).unwrap();
+
+            let mut expected_nodes = initial_nodes.clone();
+            expected_nodes.extend(tree.node(id_src).walk::<Bfs>().copied());
+            expected_nodes.sort();
+
+            let subtree = tree.node(id_src).as_cloned_subtree_within();
+            tree.node_mut(id_dst).push_child_tree_within(subtree);
+
+            let mut nodes: Vec<_> = tree.root().walk::<Bfs>().copied().collect();
             nodes.sort();
 
             assert_eq!(nodes, expected_nodes);
